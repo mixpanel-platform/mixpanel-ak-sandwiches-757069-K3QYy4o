fd002e72baa7312e40c9ff3db8b55c5a87eede2e
ak--47 <aaronkrivitzky@AaronKrivitzky-MacBookPro13.local>
2016-01-27 20:17:51 -0800

    just a tad bit of spice

--- interactions.js
+++ interactions.js
@@ -33,18 +33,23 @@ function toggleImage(containerElem) {
         if (radio.visType.value === "line") {
             clearSelect();
             $('#chooseLine').addClass('selectedImage');
+            focusSelect();
+
         }
         else if (radio.visType.value === "bar") {
             clearSelect();
             $('#chooseBar').addClass('selectedImage');
+            focusSelect();
         }
         else if (radio.visType.value === "pie") {
             clearSelect();
             $('#choosePie').addClass('selectedImage');
+            focusSelect();
         }
         else if (radio.visType.value === "table") {
             clearSelect();
             $('#chooseTable').addClass('selectedImage');
+            focusSelect();
         }
         else {
             console.log('did nothing');
@@ -67,32 +72,52 @@ function toggleImage2(containerElem) {
             $('.analysis img').removeClass('selectedImage');
         }
 
+
+
         //update the image styles
         var image = $('.analysis .toggleImage');
         if (radio.analType.value === "unique") {
             clearSelect();
             $('#chooseUniques').addClass('selectedImage');
+            focusSelect();
         }
         else if (radio.analType.value === "total") {
             clearSelect();
             $('#chooseTotals').addClass('selectedImage');
+            focusSelect();
         }
         else if (radio.analType.value === "average") {
             clearSelect();
             $('#chooseAverage').addClass('selectedImage');
+            focusSelect();
         }
         else {
-            console.log('did nothing');
+            console.log('did nothing... alariming!');
         }    
     }
 
-//the date picker
-$(document).ready(function() {
-    var dateSelector = $('<div></div>').appendTo('body').MPDatepicker();    // Create a date picker
-    dateSelector.on('change', function(e, dates) {                          // Do something when the dates are changed
-        console.log('From: ' + dates.from + '\nTo: ' + dates.to);
-    });
-});
+function focusSelect()
+{
+    $('.selectedImage').css('opacity','1');
+    $('img.toggleImage:not(.selectedImage)').css('opacity','.6');
+
+
+}
+
+
+//the date picker IS NOT WORKING
+// $(document).ready(function() {
+//     var dateSelector = $('<div></div>').appendTo('body').MPDatepicker();    // Create a date picker
+//     dateSelector.on('change', function(e, dates) {                          // Do something when the dates are changed
+//         console.log('From: ' + dates.from + '\nTo: ' + dates.to);
+//     });
+// });
+
+
+// $('#datePicker').MPDatepicker().on('change', function(dateRange) {
+//     console.log(dateRange.from);
+//     console.log(dateRange.to);
+// });
 
 
 function getDetails()
